a4c7dd073c10b28a04a303d6126375b6e1079822
sakofchit <sakun.acharige@gmail.com>
2023-03-30 22:47:52 -0400

    Initial Commit

---
+++ Back of an iPod/Back_of_an_iPodApp.swift
@@ -0,0 +1,17 @@
+//
+//  Back_of_an_iPodApp.swift
+//  Back of an iPod
+//
+//  Created by Sakun on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Back_of_an_iPodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Back of an iPodUITests/Back_of_an_iPodUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Back_of_an_iPodUITestsLaunchTests.swift
+//  Back of an iPodUITests
+//
+//  Created by Sakun on 3/30/23.
+//
+
+import XCTest
+
+final class Back_of_an_iPodUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
